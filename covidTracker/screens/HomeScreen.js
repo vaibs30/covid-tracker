8f2db7ca52f43d073c41eff96ceb29e7e9f79f26
vaibs30 <vaibhavkhatod30@gmail.com>
2020-04-07 13:08:00 +0530

    setup

---
+++ covidTracker/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import {
+  Text, View,
+} from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>sdd</Text>
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
